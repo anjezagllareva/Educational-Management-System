337839ac41dfccb69c50af5479bedea99bd9d04b
arditthoxhaa <ah56024@ubt-uni.net>
2023-04-23 15:52:41 +0200

    Courses

--- resources/views/courses.blade.php
+++ resources/views/courses.blade.php
@@ -517,7 +517,7 @@
                        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-5">
                            <div class="single-footer-caption mb-50">
                                <div class="footer-tittle">
-                                   <h4>Our solutions</h4>
+                                   <h4>Our solutions.</h4>
                                    <ul>
                                     <li><a href="#">Master the art of searching</a></li>
                                     <li><a href="#">Solve problems</a></li>
