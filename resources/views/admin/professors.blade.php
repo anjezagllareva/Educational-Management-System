6b329c0d68978cb7bb0fdf7b6c709a9b50f02d89
Anjeza <anjezagllarevaa@gmail.com>
2023-04-26 19:07:42 +0200

    .

--- resources/views/admin/professors.blade.php
+++ resources/views/admin/professors.blade.php
@@ -133,8 +133,6 @@
                             <span class="d-none d-lg-inline-flex">Admin</span>
                         </a>
                         <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
-                            <a href="#" class="dropdown-item">My Profile</a>
-                            <a href="#" class="dropdown-item">Settings</a>
                             <a href="#" class="dropdown-item">Log Out</a>
                         </div>
                     </div>
