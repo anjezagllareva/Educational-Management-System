f5f2393b2699e105631bc5102b29cab660950f1e
ardithoxhaa <ah56024@ubt-uni.net>
2023-05-08 08:19:10 +0200

    edits

--- resources/views/professor/submits.blade.php
+++ resources/views/professor/submits.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Admin</title>
+    <title>Professor</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
